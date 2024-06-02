25e3d528e500a105ba33f43bd03eac314d4a7906
Goyogooo <162113785+Goyogooo@users.noreply.github.com>
2024-06-02 20:03:53 +0800

    Add files via upload

--- BX_LW_OpenMP/BX_LW_OpenMP/test.cpp
+++ BX_LW_OpenMP/BX_LW_OpenMP/test.cpp
@@ -4,8 +4,7 @@
 #include <chrono>
 #include <algorithm>
 #include <omp.h>
-using namespace std;
-// 读取小端格式的四字节无符号整数
+
 uint32_t read_uint32_le(std::ifstream& stream) {
     uint32_t value;
     char bytes[4];
@@ -17,7 +16,6 @@ uint32_t read_uint32_le(std::ifstream& stream) {
     return value;
 }
 
-// 读取一个整数数组
 std::vector<uint32_t> read_array(std::ifstream& stream) {
     uint32_t length = read_uint32_le(stream);
     std::vector<uint32_t> array(length);
@@ -27,107 +25,62 @@ std::vector<uint32_t> read_array(std::ifstream& stream) {
     return array;
 }
 
-// 使用 OpenMP 查找两个已排序数组的交集
-vector<uint32_t> findIntersection(const vector<uint32_t>& array1, const vector<uint32_t>& array2) {
-    vector<vector<uint32_t>> local_results(omp_get_max_threads());
-
-#pragma omp parallel
-    {
-        int tid = omp_get_thread_num();
-       
-#pragma omp for nowait
-        for (int i = 0; i < array1.size(); ++i) {
-            if (binary_search(array2.begin(), array2.end(), array1[i])) {
-                local_results[tid].push_back(array1[i]);
-            }
-        }
-    }
-
-    vector<uint32_t> result;
-    for (auto& local_result : local_results) {
-        result.insert(result.end(), local_result.begin(), local_result.end());
-    }
-
-    return result;
-}
-
-
 int main() {
-    // double arg = 0;
-   // for (int j = 0; j < 1000; j++) {
     std::ifstream file("D:/MyVS/BX_LW/ExpIndex", std::ios::binary);
     if (!file) {
         std::cerr << "无法打开文件" << std::endl;
         return 1;
     }
     file.seekg(32832, std::ios::beg);
-    std::vector<uint32_t> array1 = read_array(file);
-    std::vector<uint32_t> array2 = read_array(file);
+    std::vector<uint32_t> small_array = read_array(file);
+    std::vector<uint32_t> large_array = read_array(file);
+    file.close();
 
-    vector<uint32_t> result = findIntersection(array1, array2);
+    std::sort(small_array.begin(), small_array.end());
+    std::sort(large_array.begin(), large_array.end());
 
+    std::vector<std::vector<uint32_t>> results;
 
-    /* std::vector<float> floatArray;
-        转换整数数组到浮点数组
-     for (int value : array) {
-         floatArray.push_back(static_cast<float>(value));
-     }
-     uint32_t length = floatArray.size();
-     cout << length;
-     */
-     /* std::vector<float> compress(length);
-      compress[0] = floatArray[0];
-      //auto beforeTime = std::chrono::steady_clock::now();
-      for (uint32_t i = 1; i < length; i++)
-      {
-          compress[i] = array[i] - array[i - 1];
-      }
-      */
-      // auto afterTime = std::chrono::steady_clock::now();
-       //double time = std::chrono::duration<double>(afterTime - beforeTime).count();
-      // arg += time;
-      // std::cout << " time=" << time << "seconds" << std::endl;
-      /* std::ofstream f("D:/MyVS/BX_LW/array1.txt", std::ios::app);
-       if (!f.is_open()) {
-           std::cerr << "无法打开文件" << std::endl;
-           return 0;
-       }
-       for (uint32_t value : array1) {
-           f << value << ' ';
-       }
-       f.close();
-       std::ofstream f2("D:/MyVS/BX_LW/array2.txt", std::ios::app);
-       if (!f2.is_open()) {
-           std::cerr << "无法打开文件" << std::endl;
-           return 0;
-       }
-       for (uint32_t value : array2) {
-           f2 << value << ' ';
-       }
-       f2.close();*/
+    auto beforeTime = std::chrono::steady_clock::now();
+
+    int num_threads;
+#pragma omp parallel
+    {
+#pragma omp single
+        {
+            num_threads = omp_get_num_threads();
+            results.resize(num_threads);
+        }
+
+        int thread_id = omp_get_thread_num();
+        int total_elements = large_array.size();
+        int chunk_size = total_elements / num_threads;
+        int start = thread_id * chunk_size;
+        int end = (thread_id == num_threads - 1) ? total_elements : start + chunk_size;
+
+        std::set_intersection(small_array.begin(), small_array.end(),
+            large_array.begin() + start, large_array.begin() + end,
+            std::back_inserter(results[thread_id]));
+    }
+
+    std::vector<uint32_t> final_result;
+    for (const auto& vec : results) {
+        final_result.insert(final_result.end(), vec.begin(), vec.end());
+    }
+
+    auto afterTime = std::chrono::steady_clock::now();
+    double time = std::chrono::duration<double>(afterTime - beforeTime).count();
     std::ofstream f3("D:/MyVS/BX_LW_OpenMP/result.txt", std::ios::app);
     if (!f3.is_open()) {
         std::cerr << "无法打开文件" << std::endl;
         return 0;
     }
-    for (uint32_t value : result) {
+
+    for (uint32_t value : final_result) {
         f3 << value << ' ';
     }
     f3.close();
-
-    file.close();
-    // }
-     // std::cout << " time=" << arg/100 << "seconds" << std::endl;
+    std::cout << "Intersection size: " << final_result.size() << ", time: " << time << " seconds" << std::endl;
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
